03e2e29bba149bd18ec7d6e84e4205a2383b9874
mozg3000 <i.luka@outlook.com>
2020-12-25 17:16:44 +0300

    iterator's made

--- js/dataprovider.js
+++ js/dataprovider.js
@@ -2,9 +2,37 @@ const List = require('./list.js');
 module.exports = class DataProvider{
 	constructor(obj){
 		this.data = obj.data;
+		this.filters = obj.filters
+		this.sorters = obj.sorters
+		this.filteredData = [...obj.data]
 	}
 	// Iterator
 	[Symbol.iterator](){
-		return this.data[Symbol.iterator]();
+		this.filteredData = [...this.data];
+		let filteredData = this.filters.forEach(
+			(filter) => {
+				this.filteredData = this.filteredData.filter(filter);
+			}
+		);
+		this.sorters.forEach(
+			(sorter) => this.filteredData.sort(sorter)
+		);
+		let current = this.filteredData[Symbol.iterator]();
+		const iterator = {
+			next(){
+				let next = current.next()
+				if(next.done){
+					return {
+						done: true
+					}
+				}else{
+					return {
+						done: false,
+						value: next.value
+					}
+				}
+			}
+		}
+		return iterator;
 	}
 }
